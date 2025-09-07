b8baf47c0fc16c2f77dce047d9d8837e34d8770a
なえざつく <naezatsuku653@gmail.com>
2025-09-08 00:11:17 +0900

    フードのメニューを改善。editページも変更したらpushする

--- src/components/introduction/get_food.ts
+++ src/components/introduction/get_food.ts
@@ -2,17 +2,54 @@
 
 import { serverClient } from "@/utils/supabase/server";
 
-
-
+type Data = {
+    menu:string,
+    value:number,
+    type:string,
+    image:string,
+    imageVersion:string,
+    introduction:string,
+    imageVisible:boolean
+}
+type FData = {
+    name:string
+    menu:string,
+    value:number,
+    type:string,
+    image:string,
+    imageVersion:string,
+    introduction:string,
+    imageVisible:boolean
+}
 
 export async function getFoodData(name:string) {
     const supabase = await serverClient()
 
-    const {data:food} = await supabase.from('food').select(`*`).eq("className",name );
-    console.log(food?.[0].informarion);
+    const {data:food} = await supabase.from('food').select(`information`).eq("className",name );
+    
     if(food == null) {
         return "failed"
     }
+    const Data = food?.[0].information as Data[];
+    const fData:FData[] = Data.map((value)=>{
+        const { data } = supabase.storage
+          .from('food-img')
+          .getPublicUrl(value.image);
+        let imageUrl:string = "/NOIMAGE"
+        console.log(data.publicUrl)
+        if(!value.image || value.image == ""){
+            
+        }else{
+            imageUrl = `${data.publicUrl}?v=${value.imageVersion}`;
+        }
+        
 
-    return food?.[0].information
+        return {
+            ...value,
+            name:name,
+            image:imageUrl
+        }
+    })
+    console.log(fData);
+    return fData
 }
