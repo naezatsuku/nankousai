f62f8371bde3f0177bd61191182e2f29406c3abf
なえざつく <naezatsuku653@gmail.com>
2025-09-11 01:05:47 +0900

    リデプロイ

--- src/components/event/show_event_all.tsx
+++ src/components/event/show_event_all.tsx
@@ -511,7 +511,7 @@ export default function ShowEvent(
                                 <div className="xl:w-full w-auto xl:h-auto h-full aspect-square border-l-2 border-gray-50 ">
                                     {value.img==null ?
                                         <Image placeholder={`data:image/svg+xml;base64,${toBase64(skeleton(128, 160))}`} src={"/1725741490270.jpg"}  alt="展示イラスト" width={1000} height={1000} className=" h-full w-full rounded-r-md object-cover"></Image >
-                                    :
+                                    : 
                                         <Image placeholder={`data:image/svg+xml;base64,${toBase64(skeleton(128, 160))}`} src={value.img} alt="展示イラスト" width={1000} height={1000} className=" h-full w-full rounded-r-md object-cover"></Image >
                                     }
                                     
