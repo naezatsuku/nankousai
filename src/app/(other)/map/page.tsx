634580a079c35581c0bc96c5188ddacb89f8484c
naoP-Bit <nk02kjm3805@gmail.com>
2025-09-10 12:04:35 +0900

    buildError修正

--- src/app/(other)/map/page.tsx
+++ src/app/(other)/map/page.tsx
@@ -375,7 +375,7 @@ export default function Page() {
                                             }
                                             
                                         {modal_data?.data?.map((modal_value, modal_index) => 
-                                            <Link href={{pathname:"/event/introduction", query:{name:modal_value.className}}}>
+                                            <Link href={{pathname:"/event/introduction", query:{name:modal_value.className}}} key={modal_index}>
                                                 <div key={"modal:" + String(modal_index)} className="px-5">
                                                     <div className={`${(modal_index == 0 || modal_data.data.length == 0) ? "" : "border-t"} border-gray-400 py-3 flex justify-between items-center ${modal_index == modal_data.data.length - 1 && "pb-3 md:pb-4"}`} key={"modal_data"+ String(modal_index)}>
                                                         <div className=" overflow-hidden pr-2">
@@ -417,7 +417,7 @@ export default function Page() {
                                             </div>
                                         }
                                         {modal_data?.data?.map((modal_value, modal_index) => 
-                                        <Link href={{pathname:"/event/introduction", query:{name:modal_value.className}}}>
+                                        <Link href={{pathname:"/event/introduction", query:{name:modal_value.className}}} key={modal_index}>
                                             <div key={"modal:" + String(modal_index)} className="px-4 md:px-5">
                                                 <div className={`${(modal_index == 0 || modal_data.data.length == 0) ? "" : "border-t"} border-gray-400 py-2 flex justify-between items-center ${modal_index == modal_data.data.length - 1 && "pb-3 md:pb-4"}`} key={"modal_data"+ String(modal_index)}>
                                                     <div className="overflow-hidden ">
