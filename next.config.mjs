59d28088fc46815930a31c709f194d6352d3f924
naezatsuku <naezatsuku653@gmail.com>
2025-09-12 12:16:25 +0900

    画像最適化on

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
   images: {
     domains: ["fonts.gstatic.com","myypelzqjunsrpytkiee.supabase.co","cdn.toolpods.io"],
-    loader: "custom",
-    loaderFile: "./src/imageLoader.ts",
-    unoptimized: true
      // ここでフォント取得を許可
   },
   async headers() {
